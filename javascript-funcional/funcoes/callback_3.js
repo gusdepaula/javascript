e6d443640f16cd4765eb1b52f914dc5bd6042c19
Gustavo de Paula Santos <gustavo.paula@certisign.com.br>
2020-06-25 15:26:41 -0300

    Função Map #03

--- javascript-funcional/funcoes/callback_3.js
+++ javascript-funcional/funcoes/callback_3.js
@@ -12,3 +12,18 @@ const primeiraLetra = letra => letra[0]
 const letras = nome.map(primeiraLetra)
 
 console.log(nome, letras)
+
+const carrinho = [
+    { nome: 'Caneta', qtde: 10, preco: 7.99 },
+    { nome: 'Impressora', qtde: 0, preco: 649.50 },
+    { nome: 'Caderno', qtde: 4, preco: 27.10 },
+    { nome: 'Lapis', qtde: 3, preco: 5.82 },
+    { nome: 'Tesoura', qtde: 1, preco: 19.20 }
+]
+
+const nomeProduto = n => n.nome
+console.log(carrinho.map(nomeProduto))
+
+const qtdeXPreco = x => x.qtde * x.preco
+const totais = carrinho.map(qtdeXPreco)
+console.log(totais)

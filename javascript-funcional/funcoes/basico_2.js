aa1eb5d6fd7d6e184f803df7b09e63296a846005
Gustavo de Paula Santos <gustavo.paula@certisign.com.br>
2020-06-25 11:23:01 -0300

    Básico sobre função #02

---
+++ javascript-funcional/funcoes/basico_2.js
@@ -0,0 +1,31 @@
+function bomDia(){
+    console.log('Bom dia!')
+}
+
+const boaTarde = function () {
+    console.log('Bom tarde!')
+}
+
+//Passar uma função como parametro para outra função
+function executarQualquerCoisa(fn) {
+    if(typeof fn === 'function') {
+        fn()
+    }
+}
+
+executarQualquerCoisa(3)
+executarQualquerCoisa(bomDia)
+executarQualquerCoisa(boaTarde)
+
+// Retornar uma função a partir de outra função
+function potencia(base) {
+    return function(exp) {
+        return Math.pow(base, exp)
+    }
+}
+
+const potenciaDe2 = potencia(2)
+console.log(potenciaDe2(8))
+
+const pot34 = potencia(3)(4)
+console.log(pot34)

cce9efac6d0f9780290acb032f6e8dd02876a1e8
Gustavo de Paula Santos <gustavo.paula@certisign.com.br>
2020-06-26 11:46:14 -0300

    Função Filter #03

--- javascript-funcional/funcoes/callback_4.js
+++ javascript-funcional/funcoes/callback_4.js
@@ -21,4 +21,20 @@ const qtdeMaiorQueZero = item => item.qtde > 0
 const itensComEstoque = carrinho
     .filter(qtdeMaiorQueZero)
     .map(nomeProduto)
-console.log('Esses itens temos estoque', itensComEstoque)
+console.log('Esses itens temos estoque', itensComEstoque.toString())
+
+//CRIANDO MEU PROPRIO FILTER
+Array.prototype.meuFilter = function (fn) {
+    const novoArray = []
+    for (let i = 0; i < this.length; i ++) {
+        if(fn(this[i], i, this)) {
+            novoArray.push(this[i])
+        }
+    }
+    return novoArray
+}
+
+const itensComEstoque2 = carrinho
+    .meuFilter(qtdeMaiorQueZero)
+    .map(nomeProduto)
+console.log('Filtrei com meu filter e esses itens temos estoque', itensComEstoque2.toString())

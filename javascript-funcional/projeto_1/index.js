b164caaad14490de58d6e9b65aca60f7dc6e55b0
Gustavo de Paula Santos <gustavo.paula@certisign.com.br>
2020-07-01 11:28:08 -0300

    Usando Promise

--- javascript-funcional/projeto_1/index.js
+++ javascript-funcional/projeto_1/index.js
@@ -3,5 +3,5 @@ const path = require('path')
 
 const caminho = path.join(__dirname, '..', 'dados', 'legendas')
 
-const arquivos = fn.leituraArquivo(caminho)
-console.log(arquivos)
+fn.leituraArquivo(caminho)
+    .then(console.log)

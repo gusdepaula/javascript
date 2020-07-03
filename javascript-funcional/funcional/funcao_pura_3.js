33e3c26c4a7470790a27da555c2103215b3dbf0b
Gustavo de Paula Santos <gustavo.paula@certisign.com.br>
2020-07-03 14:46:47 -0300

    Função pura #03

---
+++ javascript-funcional/funcional/funcao_pura_3.js
@@ -0,0 +1,17 @@
+// Uma funcao pura é uma funcao em que o valor 
+// de retorno é determinado APENAS por seus valores 
+// de entrada, sem efeitos colaterais observáveis
+
+let qtdeDeExecucoes = 0
+
+//funcao pura
+function somar(a, b){
+    qtdeDeExecucoes++ //efeitos colaterais observáveis
+    return a + b
+}
+
+console.log(qtdeDeExecucoes)
+console.log(somar(68, 31))
+console.log(somar(68, 31))
+console.log(somar(68, 31))
+console.log(qtdeDeExecucoes)

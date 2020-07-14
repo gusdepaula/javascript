dc922982420fb471bf2cfdcef59fa25df17fb09a
Gustavo de Paula Santos <gustavo.paula@certisign.com.br>
2020-07-14 10:37:28 -0300

    Functors

---
+++ javascript-funcional/funcional/functors.js
@@ -0,0 +1,38 @@
+// Functors são objetos que implementam a função MAP
+// que também é um "wrapper" de um valor
+
+
+//ARRAY é um exemplo de Functors
+const nums = [1, 2, 3, 4, 5, 6]
+const novoNums = nums
+    .map(el => el + 10)
+    .map(el => el * 2)
+
+console.log(nums, novoNums)
+
+function TipoSeguro(valor){
+    return {
+        valor: valor,
+        invalido(){
+            return this.valor === null || this.valor === undefined
+        },
+        map(fn){
+            if(this.invalido()) {
+                return TipoSeguro(null)
+            } else {
+                const novoValor = fn(this.valor)
+                return TipoSeguro(novoValor)
+            }
+            
+        }
+    }
+}
+
+const original = 'Esse é um texto'
+
+const alterado = TipoSeguro(original)
+    .map(t => t.toUpperCase())
+    .map(t => `${t}!!!`)
+    .map(t => t.split('').join(' '))
+
+    console.log(original, alterado.valor)    

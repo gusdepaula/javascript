dfa81952e9158100af82606a3140a35438cf4396
Gustavo de Paula Santos <gustavo.paula@certisign.com.br>
2020-07-15 11:21:35 -0300

    Padrão Observer

---
+++ javascript-funcional/observer/festa_surpresa.js
@@ -0,0 +1,44 @@
+const readline = require('readline')
+
+function obterResposta(pergunta) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+    return new Promise(resolve => {
+        rl.question(pergunta, resp => {
+            resolve(resp)
+            rl.close()
+        })
+    })
+}
+
+function namorada(evento) {
+    console.log('N: Apagar as luzes')
+    console.log('N: Pedir silêncio')
+    console.log('N: Surpresa')
+}
+
+function sindico(evento) {
+    console.log('S: Monitorando o barulho')
+    console.log(`S: evento -> ${evento.resp}`)
+    console.log(`S: evento -> ${evento.data}`)
+}
+
+async function porteiro(interessados) {
+    while(true) {
+        const resp = await obterResposta('O namorado chegou? (s/N/q)')
+        if(resp.toLowerCase() === 's') {
+            // os observadores são notificados
+            (interessados || []).forEach(obs => obs({resp, data: Date.now() }))
+        } else if(resp.toLowerCase() === 'q') {
+            break
+        }
+    }
+}
+/*
+    Chamada da função  -> registra os observadores!
+    Os observadores são: namorada e sindíco
+    O subject é o porteiro!
+*/ 
+porteiro([namorada, sindico]) 

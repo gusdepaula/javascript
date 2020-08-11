a270b352c7cd35a254c39b7e54b1cac5a7f4fce8
Gustavo de Paula Santos <gustavo.paula@certisign.com.br>
2020-08-11 16:16:07 -0300

    toBeDefined

--- testes-unitarios/jasmine-standalone-3.6.0/spec/toMatchSpec.js
+++ testes-unitarios/jasmine-standalone-3.6.0/spec/toMatchSpec.js
@@ -1,7 +1,7 @@
 describe('Teste do toMatch', function(){
     it("Deve validar o uso do toMatch'", function() {
         var texto = "teste com Jasmine";
-        var obj2 = { valor: true };
+        
         expect(texto).toMatch("Jasmine");
         expect(texto).toMatch(/jasmine/i);
         expect("12345-456").toMatch(/^\d{5}-\d{3}$/);

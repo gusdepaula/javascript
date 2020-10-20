f25f1fdb7fec3ada4826fc9809d5becc857ab890
Gustavo de Paula <gdepaula@gmail.com>
2020-10-20 18:16:26 -0300

    Values and Variable

--- javascript-course/01-Fundamentals-Part-1/script.js
+++ javascript-course/01-Fundamentals-Part-1/script.js
@@ -1,5 +1,19 @@
 let js = 'amazing';
-if (js === 'amazing') alert('Javascript is FUN!');
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);

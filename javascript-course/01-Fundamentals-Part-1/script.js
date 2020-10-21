5fa9bbb400bd4c4e766661bb0cc2228a4dbb9647
Gustavo de Paula <gdepaula@gmail.com>
2020-10-21 16:41:40 -0300

    Operator Precedence

--- javascript-course/01-Fundamentals-Part-1/script.js
+++ javascript-course/01-Fundamentals-Part-1/script.js
@@ -42,7 +42,7 @@ job = 'Teacher';
 
 lastName = 'De Paula';
 console.log(lastName);
-*/
+
 
 // Math operators
 const now = 2037;
@@ -72,3 +72,17 @@ console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const avaregeAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, avaregeAge);

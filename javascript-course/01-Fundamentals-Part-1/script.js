a8f9948045d78406d803efdcb5f88c4fb0d494e6
Gustavo de Paula <gdepaula@gmail.com>
2020-10-21 11:40:41 -0300

    let, const and var

--- javascript-course/01-Fundamentals-Part-1/script.js
+++ javascript-course/01-Fundamentals-Part-1/script.js
@@ -18,13 +18,28 @@ let myFirstJob = "Programmer";
 let myCurrentJob = "Teacher";
 
 console.log(myFirstJob);
-*/
 
 let country = "Brazil";
 let continent = "South America";
-let population = "209.000.000"
-console.log(`I live in ${country}. ${country} is located in ${continent} and have a population of ${population} millions of people.`)
+let population = "209.000.000";
 
 let javascriptIsFun = true;
 console.log(`Javascript is fun? ${javascriptIsFun}`);
 
+let isIsland = false;
+console.log(`I live in ${country}. ${country} is located in ${continent} and have a population of ${population} millions of people. ${country} is a island? Answer: ${isIsland}`)
+
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'Programmer';
+job = 'Teacher';
+
+lastName = 'De Paula';
+console.log(lastName);

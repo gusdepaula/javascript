deb32a786e3cd6e04dc1e1dbe0e3a7e603b9e6dd
Gustavo de Paula <gdepaula@gmail.com>
2020-10-23 16:37:54 -0300

    Functions

--- javascript-course/02-Fundamentals-Part-2/script.js
+++ javascript-course/02-Fundamentals-Part-2/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 let hasDriversLicense = true;
 const passTest = true;
 
@@ -8,3 +8,25 @@ if (hasDriversLicense) console.log('I can drive');
 
 // const interface = 'Audio';
 // const private = 534;
+
+*/
+
+function logger() {
+    console.log('My name is Gustavo');
+}
+
+// calling, running or invoking the function
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges}.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');

37611b8595e788f14f7d38d1dbd2247bb59775ba
Gustavo de Paula <gdepaula@gmail.com>
2020-10-29 15:29:29 -0300

    Implementing the Game Logic

--- javascript-course/05-Guess-My-Number/script.js
+++ javascript-course/05-Guess-My-Number/script.js
@@ -9,8 +9,13 @@ document.querySelector('.score').textContent = '20';
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
-🎉
+
 */
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -18,5 +23,25 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number';
+  } else if (guess == secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 GAME OVER';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 GAME OVER';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });

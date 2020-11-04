323c2f4c0aae4eefd4fdf1b35e64f9afe8ddb882
Gustavo de Paula <gdepaula@gmail.com>
2020-11-04 18:53:05 -0300

    Regular Functions vs. Arrow Functions

--- javascript-course/08-Behind-the-Scenes/script.js
+++ javascript-course/08-Behind-the-Scenes/script.js
@@ -82,7 +82,7 @@ const z = 3;
 console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
- */
+
 
 console.log(this);
 
@@ -119,3 +119,51 @@ matilda.calcAge();
 
 const f = gustavo.calcAge;
 f();
+ */
+
+// var firstName = 'Matilda';
+
+const gustavo = {
+  firstName: 'Gustavo',
+  year: 1983,
+  calcAge: function () {
+    // console.log(this);
+    console.log(2037 - this.year);
+
+    // Solution 1
+    // const self = this; // self or that
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1983 && self.year <= 1996);
+    // };
+
+    // Solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1983 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+
+gustavo.greet();
+gustavo.calcAge();
+
+// arguments keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);

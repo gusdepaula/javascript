8722c9b9e58f108f6f2e6d55739d9e9933c412b6
Gustavo de Paula <gdepaula@gmail.com>
2020-11-10 18:20:35 -0300

    Working with Strings - Part 3

--- javascript-course/09-Data-Structures-Operators/script.js
+++ javascript-course/09-Data-Structures-Operators/script.js
@@ -85,6 +85,55 @@ const game = {
   },
 };
 
+// Split and join
+console.log('a+very+nice+string'.split('+'));
+console.log('Gustavo Santos'.split(' '));
+
+const [firstName, lastName] = 'Gustavo Santos'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('gustavo de paula santos');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(25, '+').padEnd(30, '+'));
+console.log('Gustavo'.padStart(25, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(4984123456781234));
+console.log(maskCreditCard('498412345678123473733'));
+
+// Repeat
+const message2 = 'Bad weather...All departures delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'✈️'.repeat(n)} `);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+/*
+// Working with Strings - Part 2
 const airline = 'TAP Air Portugal';
 console.log(airline.toLowerCase());
 console.log(airline.toUpperCase());
@@ -145,7 +194,7 @@ checkBaggage('I have a laptop, some Food and a pocket Knife');
 checkBaggage('Socks and camera');
 checkBaggage('Got some snacks and a gun for protection');
 
-/*
+
 // Working With Strings - Part 1
 const airline = 'TAP Air Portugal';
 const plane = 'A320';

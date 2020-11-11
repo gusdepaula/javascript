e8a5947ba8b8f0ffed644dff22cd541d739ed8c6
Gustavo de Paula <gdepaula@gmail.com>
2020-11-11 14:43:25 -0300

    Functions Accepting Callback Functions

--- javascript-course/10-Functions/script.js
+++ javascript-course/10-Functions/script.js
@@ -26,7 +26,7 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
-*/
+
 
 //How Passing Arguments Works: Value vs. Reference
 const flight = 'LH1234';
@@ -60,3 +60,34 @@ const newPassport = function (person) {
 
 newPassport(gustavo);
 checkIn(flight, gustavo);
+*/
+
+// Functions Accepting Callback Functions
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWork = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// High-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('Javascript is the best!', upperFirstWork);
+
+transformer('Javascript is the best!', oneWord);
+
+// Js uses callbacks all the time
+const high5 = function () {
+  console.log('👋');
+};
+
+document.body.addEventListener('click', high5);
+['Jonas', 'Martha', 'Adam'].forEach(high5);

dc252582d930c0f0163fa08b45f5a0edf1743b13
Gustavo de Paula <gdepaula@gmail.com>
2020-11-16 18:54:04 -0300

    The Magic of Chaining Methods

--- javascript-course/11-Arrays-Bankist/script.js
+++ javascript-course/11-Arrays-Bankist/script.js
@@ -71,7 +71,7 @@ const displayMovements = function (movements) {
           <div class="movements__type movements__type--deposit">${
             i + 1
           } ${type}</div>
-          <div class="movements__value">${mov}</div>
+          <div class="movements__value">${mov}€</div>
         </div>
       `;
 
@@ -80,6 +80,35 @@ const displayMovements = function (movements) {
 };
 
 displayMovements(account1.movements);
+
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance}€`;
+};
+
+calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+calcDisplaySummary(account1.movements);
 
 const createUsernames = function (accs) {
   accs.forEach(function (account) {
@@ -92,13 +121,6 @@ const createUsernames = function (accs) {
 };
 
 createUsernames(accounts);
-
-const calcDisplayBalance = function (movements) {
-  const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
-};
-
-calcDisplayBalance(account1.movements);
 
 //console.log(containerMovements.innerHTML);
 /////////////////////////////////////////////////
@@ -326,7 +348,7 @@ Test data:
 § Data 1: [5, 2, 4, 1, 15, 8, 3]
 § Data 2: [16, 6, 10, 5, 6, 1, 4]
 GOOD LUCK
-*/
+
 
 const calcAverageHumanAge = function (ages) {
   const humanAges = ages.map(function (dogAge) {
@@ -371,3 +393,17 @@ console.log(`--Data 1: [5, 2, 4, 1, 15, 8, 3]--`);
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 console.log(`--Data 2: [16, 6, 10, 5, 6, 1, 4]--`);
 calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+*/
+const eurToUsd = 1.1;
+console.log(movements);
+
+// Pipeline
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  //   .map(mov => mov * eurToUsd)
+  .map((mov, i, arr) => {
+    // console.log(arr);
+    return mov * eurToUsd;
+  })
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);

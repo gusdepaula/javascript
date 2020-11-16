e3b72f175639cbe02f469221a737a37c09b9c0ae
Gustavo de Paula <gdepaula@gmail.com>
2020-11-16 16:19:02 -0300

    Coding Challenge #2

--- javascript-course/11-Arrays-Bankist/script.js
+++ javascript-course/11-Arrays-Bankist/script.js
@@ -279,7 +279,7 @@ const withdrawals = movements.filter(function (mov) {
   return mov < 0;
 });
 console.log(withdrawals);
-*/
+
 
 console.log(movements);
 
@@ -304,3 +304,70 @@ const max = movements.reduce((acc, mov) => {
   }
 }, movements[0]);
 console.log(max);
+
+///////////////////////////////////////////////////////////
+Coding Challenge #2
+
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert
+dog ages to human ages and calculate the average age of the dogs in their study.
+Your tasks:
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's
+ages ('ages'), and does the following things in order:
+1. Calculate the dog age in human years using the following formula: if the dog is
+<= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old,
+humanAge = 16 + dogAge * 4
+2. Exclude all dogs that are less than 18 human years old (which is the same as
+keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know
+from other challenges how we calculate averages �)
+4. Run the function for both test datasets
+Test data:
+§ Data 1: [5, 2, 4, 1, 15, 8, 3]
+§ Data 2: [16, 6, 10, 5, 6, 1, 4]
+GOOD LUCK
+*/
+
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map(function (dogAge) {
+    if (dogAge <= 2) {
+      return 2 * dogAge;
+    } else {
+      return 16 + dogAge * 4;
+    }
+    // if (dogAge <= 2) {
+    //   console.log(
+    //     `Dog age is ${dogAge} and human age is ${2 * dogAge} years old`
+    //   );
+    // } else if (dogAge > 2) {
+    //   console.log(
+    //     `Dog age is ${dogAge} and human age is ${16 + dogAge * 4} years old`
+    //   );
+    // }
+  });
+
+  const adults = humanAges.filter(function (dogAge) {
+    if (dogAge >= 18) {
+      return dogAge;
+    }
+  });
+  //   const adults = ages.filter(function (dogAge) {
+  //     const adultDog = 16 + dogAge * 4;
+  //     if (dogAge > 2) {
+  //       console.log(`Dog ${dogAge} is not a puppy`);
+  //     }
+  //   });
+
+  const average = adults.reduce(function (acc, dogAge) {
+    return acc + dogAge / adults.length;
+  }, 0);
+
+  console.log(humanAges);
+  console.log(adults);
+  console.log(average);
+};
+
+console.log(`--Data 1: [5, 2, 4, 1, 15, 8, 3]--`);
+calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+console.log(`--Data 2: [16, 6, 10, 5, 6, 1, 4]--`);
+calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);

0276cd53116ac74734bcd1e27ed3734aa217ccf1
Gustavo de Paula <gdepaula@gmail.com>
2020-11-16 11:34:52 -0300

    The map Method

--- javascript-course/11-Arrays-Bankist/script.js
+++ javascript-course/11-Arrays-Bankist/script.js
@@ -81,12 +81,12 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
-console.log(containerMovements.innerHTML);
+//console.log(containerMovements.innerHTML);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -195,7 +195,7 @@ Test data:
 § Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
 Hints: Use tools from all lectures in this section so far �
 GOOD LUCK �
-*/
+
 
 const checkDogs = function (dogsJulia, dogsKate) {
   const dogsJuliaCorrected = dogsJulia.slice();
@@ -219,3 +219,28 @@ console.log(`--------------Data 1--------------`);
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 console.log(`--------------Data 2--------------`);
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+*/
+
+const eurToUsd = 1.1;
+
+// const movementsUSD = movements.map(function (mov) {
+//     return mov * eurToUsd;
+//   });
+
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
+
+const movementsDescripitions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposite' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+
+console.log(movementsDescripitions);

2bf133f3a16758429c60296aa992ab44bd1b0c66
Gustavo de Paula <gdepaula@gmail.com>
2020-11-18 12:02:40 -0300

    More Ways of Creating and Filling Arrays

--- javascript-course/11-Arrays-Bankist/script.js
+++ javascript-course/11-Arrays-Bankist/script.js
@@ -590,7 +590,7 @@ const overalBalance2 = accounts
   .flatMap(account => account.movements)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(overalBalance2);
-*/
+
 
 // Strings
 const owners = [];
@@ -619,3 +619,32 @@ console.log(movements);
 // });
 movements.sort((a, b) => b - a);
 console.log(movements);
+*/
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+const x = new Array(7);
+console.log(x);
+x.fill(1);
+console.log(x);
+
+arr.fill(23, 4, 6);
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (curr, i) => i + 1);
+console.log(z);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementsUI);
+
+  const movementsUI2 = [...document.querySelectorAll('.movements__value')];
+  console.log(movementsUI2);
+});

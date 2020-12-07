e622785981a76d74e4dd86dd8546c2d119b77a82
Gustavo de Paula <gdepaula@gmail.com>
2020-12-07 16:29:19 -0300

    Creating Dates

--- javascript-course/12-Numbers-Dates-Timers-Bankist/script.js
+++ javascript-course/12-Numbers-Dates-Timers-Bankist/script.js
@@ -354,8 +354,6 @@ labelBalance.addEventListener('click', function(){
   })
 });
 
-*/
-
 console.log(2 ** 53 - 1);
 console.log(Number.MAX_SAFE_INTEGER);
 console.log(2 ** 53 + 1);
@@ -385,3 +383,38 @@ console.log(huge + ' is REALLY big!!!');
 // Divisions
 console.log(11n / 3n);
 console.log(10 / 3);
+
+// Create a date
+const now = new Date();
+console.log(now);
+
+console.log(new Date('Dec 07 2020 16:17:53'));
+console.log(new Date('December 24, 2015'));
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 31));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+*/
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142267780000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
+

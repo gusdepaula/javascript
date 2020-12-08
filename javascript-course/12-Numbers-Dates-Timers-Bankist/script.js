032f16ecfcc306ae488b28901adc938e6323549e
Gustavo de Paula <gdepaula@gmail.com>
2020-12-08 12:54:32 -0300

    Implementing a Countdown Timer

--- javascript-course/12-Numbers-Dates-Timers-Bankist/script.js
+++ javascript-course/12-Numbers-Dates-Timers-Bankist/script.js
@@ -179,14 +179,42 @@ const updateUI = function (acc) {
   calcDisplaySummary(acc);
 };
 
+const startLogOutTimer = function(){
+  const tick = function(){
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const sec = String(time % 60).padStart(2,0);
+
+    // In each call print the remaining time to UI
+    labelTimer.textContent = `${min}:${sec}`;
+
+    // When 0 seconds, stop timer and log out user
+    if(time === 0) {
+      clearInterval(timer);
+      labelWelcome.textContent = `Log in to get started`;
+      containerApp.style.opacity = 0;
+    }
+
+     // Decrase 1s
+     time--;
+  }
+
+  // Set time to 5 minutes
+  let time = 120;
+
+  // Call the timer every second
+  tick();
+  const timer = setInterval(tick, 1000);
+  return timer;
+};
+
 ///////////////////////////////////////
 // Event handlers
-let currentAccount;
+let currentAccount, timer;
 
 // FAKE ALWAYS LOGGED IN
-currentAccount = account1;
-updateUI(currentAccount)
-containerApp.style.opacity = 100;
+// currentAccount = account1;
+// updateUI(currentAccount)
+// containerApp.style.opacity = 100;
 
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
@@ -217,7 +245,7 @@ btnLogin.addEventListener('click', function (e) {
 
     // const locale = navigator.language;
 
-    labelDate.textContent = new Intl.DateTimeFormat(currentAccount.locale, options).format(now)
+    labelDate.textContent = new Intl.DateTimeFormat(currentAccount.locale, options).format(now);
     // const day = `${now.getDate()}`.padStart(2, '0');
     // const month = `${now.getMonth() + 1}`.padStart(2, '0');
     // const year = now.getFullYear();
@@ -228,6 +256,10 @@ btnLogin.addEventListener('click', function (e) {
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
+
+    // Timer 
+    if(timer) clearInterval(timer);
+    timer = startLogOutTimer();
 
     // Update UI
     updateUI(currentAccount);
@@ -258,6 +290,10 @@ btnTransfer.addEventListener('click', function (e) {
 
     // Update UI
     updateUI(currentAccount);
+
+    // Reset timer
+    clearInterval(timer);
+    timer = startLogOutTimer();
   }
 });
 
@@ -276,6 +312,10 @@ btnLoan.addEventListener('click', function (e) {
 
       // Update UI
       updateUI(currentAccount);
+
+      // Reset timer
+      clearInterval(timer);
+      timer = startLogOutTimer();
     }, 2500);
   }
   inputLoanAmount.value = '';
@@ -501,7 +541,6 @@ console.log('Germany: ', new Intl.NumberFormat('de-DE', options).format(num));
 console.log('Syria: ', new Intl.NumberFormat('ar-SY', options).format(num));
 console.log('Browse: ', new Intl.NumberFormat(navigator.language, options).format(num));
 
-*/
 // setTimeout
 const ingredients = ['olives', 'spinach']
 
@@ -518,3 +557,4 @@ setInterval(function(){
   const now = new Date();
   console.log(now);
 }, 3000);
+*/

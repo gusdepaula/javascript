5f6460e2a81c480a939c22c516c2ec8e80f89409
Gustavo de Paula <gdepaula@gmail.com>
2020-12-14 15:06:31 -0300

    Lazy Loading Images

--- javascript-course/13-Advanced-DOM-Bankist/script.js
+++ javascript-course/13-Advanced-DOM-Bankist/script.js
@@ -163,7 +163,6 @@ const allSections = document.querySelectorAll('.section');
 
 const revealSection = function(entries, observer) {
   const [entry] = entries;
-  console.log(entry);
 
   if(!entry.isIntersecting){
     return;
@@ -172,17 +171,45 @@ const revealSection = function(entries, observer) {
     observer.unobserve(entry.target);
   }
   
-}
+};
 
 const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
   threshold: 0.15,
-})
+});
 
 allSections.forEach(function(section){
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
-})
+});
+
+// Lazy loading images
+const imgTarget = document.querySelectorAll('img[data-src]');
+
+const loadImg = function(entries, observer) {
+  const [entry] = entries;
+  
+  if(!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function() {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTarget.forEach(function(img) {
+  imgObserver.observe(img);
+});
 
 /*
 console.log(document.documentElement);

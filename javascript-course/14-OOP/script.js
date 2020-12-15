097b05647bbbc59992d2291a92fbef23cb0f5e8d
Gustavo de Paula <gdepaula@gmail.com>
2020-12-15 15:06:37 -0300

    ES6 Classes

--- javascript-course/14-OOP/script.js
+++ javascript-course/14-OOP/script.js
@@ -78,7 +78,7 @@ DATA CAR 1: 'BMW' going at 120 km/h
 DATA CAR 2: 'Mercedes' going at 95 km/h
 
 GOOD LUCK 😀
-*/
+
 
 
 const Car = function(make, speed) {
@@ -102,3 +102,39 @@ bmw.brake();
 
 mercedes.accelerate();
 mercedes.brake();
+
+*/
+
+// class expression
+// const PersonCl = class {}
+
+// class declaration
+class PersonCl {
+    constructor(firstName, birthYear) {
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    }
+    // Methods will be added to .prototype property
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    }
+
+    greet() {
+        console.log(`Hey ${this.firstName}`);
+    }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function(){
+//     console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class citizens
+// 3. Classes are executed in strict mode

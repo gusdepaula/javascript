fe6ffccd0bbd60bbefb89d37c7f7c1bf964fdd87
Gustavo de Paula <gdepaula@gmail.com>
2020-12-16 10:24:23 -0300

    Coding Challenge #2

--- javascript-course/14-OOP/script.js
+++ javascript-course/14-OOP/script.js
@@ -112,7 +112,7 @@ mercedes.brake();
 // class expression
 // const PersonCl = class {}
 
-// class declaration
+/*// class declaration
 class PersonCl {
     constructor(fullName, birthYear) {
         this.fullName = fullName;
@@ -212,4 +212,49 @@ console.log(steven.__proto__ === PersonProto);
 
 const sarah = Object.create(PersonProto);
 sarah.init('Sarah', 1979);
-sarah.calcAge();
+sarah.calcAge(); */
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+
+DATA CAR 1: 'Ford' going at 120 km/h
+
+GOOD LUCK 😀
+*/
+
+class CarCl {
+    constructor(make, speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+
+    
+    accelerate(){
+        this.speed + 10;
+    };
+
+    brake() {
+        this.speed - 5;
+    };
+
+    get speedUS(){
+        return this.speed / 1.6;
+    }
+
+    // Set a property that already exists
+    set speedUS(speed) {
+        return this._speed = speed * 1.6;
+    }
+}
+
+const ford = new CarCl('Ford', 120);
+console.log(ford.speedUS);
+console.log(ford);
+ford.speedUS = 50;
+console.log(ford);

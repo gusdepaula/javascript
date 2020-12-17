96cade1787a7eb2b7ed8c45c6d93bf5f74f95008
Gustavo de Paula <gdepaula@gmail.com>
2020-12-17 09:23:23 -0300

    Encapsulation: Protected Properties and Methods

--- javascript-course/14-OOP/script.js
+++ javascript-course/14-OOP/script.js
@@ -457,26 +457,31 @@ class Account {
     constructor(owner, currency, pin) {
         this.owner = owner;
         this.currency = currency;
-        this.pin = pin;
-        this.movements = [];
+        this._pin = pin;
+        // Protected Property
+        this._movements = [];
         this.locale = navigator.language;
         console.log(`Thanks for opening an acccount, ${owner}`);
     }
 
+    getMovements() {
+        return this._movements;
+    }
+
     deposit(val) {
-        this.movements.push(val)
+        this._movements.push(val)
     }
 
     withdraw(val) {
         this.deposit(-val)
     }
 
-    aprroveLoan(val){
+    _aprroveLoan(val){
         return true;
     }
 
     requestLoan(val) {
-        if(this.aprroveLoan(val)) {
+        if(this._aprroveLoan(val)) {
             this.deposit(val);
             console.log(`Loan approved`);
         }
@@ -484,10 +489,15 @@ class Account {
 }
 
 const acc1 = new Account('Gustavo', 'BRL', 1111);
+
+
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
-acc1.aprroveLoan(1000);
-// acc1.movements.push(250);
-// acc1.movements.push(-140);
+acc1._aprroveLoan(1000);
+console.log(acc1.getMovements());
+
 console.log(acc1);
+console.log(acc1.pin);

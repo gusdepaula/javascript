5368ca41f5622bc34577b3c84ab1b08612fb9655
Gustavo de Paula <gdepaula@gmail.com>
2020-12-23 15:58:35 -0300

    The Event Loop in Practice

--- javascript-course/16-Asynchronous/script.js
+++ javascript-course/16-Asynchronous/script.js
@@ -179,7 +179,7 @@ TEST COORDINATES 2: 19.037, 72.873
 TEST COORDINATES 2: -33.933, 18.474
 
 GOOD LUCK 😀
-*/
+
 
 const whereAmI = function (lat, lng) {
   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
@@ -208,3 +208,18 @@ const whereAmI = function (lat, lng) {
 whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
+*/
+
+console.log('Test start');
+
+setTimeout(() => {
+  console.log('0 sec timer');
+}, 0);
+
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolve promise 2').then(res => {
+  for (let i = 0; i < 100000000; i++) {}
+  console.log(res);
+});
+console.log('Test end');

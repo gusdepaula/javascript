c43081011b6686fe5a706ff78658f865f87d2add
Gustavo de Paula <gdepaula@gmail.com>
2020-12-28 15:42:46 -0300

    Exporting and Importing in ES6 Modules

---
+++ javascript-course/17-Modern-JS-Modules-Tooling/script.js
@@ -0,0 +1,25 @@
+// Importing module
+// import {
+//   addToCart,
+//   totalPrice as price,
+//   totalQuantity as tq,
+// } from './shoppingCart.js';
+// console.log(shippingCost);
+// addToCart('bread', 5);
+// console.log(price, tq);
+
+console.log('Importing module');
+
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+// import add, { totalPrice as price, tq } from './shoppingCart.js';
+// console.log(price);
+
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
+
+console.log(cart);

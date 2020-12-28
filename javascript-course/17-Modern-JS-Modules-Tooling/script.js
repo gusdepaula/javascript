b4e807e91d96e244bd2994d09eb962d1014a5139
Gustavo de Paula <gdepaula@gmail.com>
2020-12-28 18:04:49 -0300

    The Module Pattern

--- javascript-course/17-Modern-JS-Modules-Tooling/script.js
+++ javascript-course/17-Modern-JS-Modules-Tooling/script.js
@@ -1,3 +1,4 @@
+/*
 // Importing module
 // import {
 //   addToCart,
@@ -23,3 +24,35 @@ add('bread', 5);
 add('apples', 4);
 
 console.log(cart);
+*/
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart(shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 4);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);

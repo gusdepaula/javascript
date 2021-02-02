0e43cee88b4473f80dda912ba487fd6814feb0e9
Gustavo de Paula <gdepaula@gmail.com>
2021-02-02 12:43:13 -0300

    Array Methods Pratice - example 2

--- javascript-course/11-Arrays-Bankist/script.js
+++ javascript-course/11-Arrays-Bankist/script.js
@@ -661,6 +661,22 @@ const bankDepositSum = accounts
 
 console.log(bankDepositSum);
 
+// 2.
+// const numDeposits1000 = accounts
+//   .flatMap(acc => acc.movements)
+//   .filter(mov => mov >= 1000).length;
+
+const numDeposits1000 = accounts
+  .flatMap(acc => acc.movements)
+  // .reduce((count, cur) => (cur >= 1000 ? count + 1 : count), 0);
+  .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
+
+console.log(numDeposits1000);
+
+// Prefixed ++ operator
+let a = 10;
+console.log(++a);
+
 /*
 //////////////////////////////////////
 Coding Challenge #4

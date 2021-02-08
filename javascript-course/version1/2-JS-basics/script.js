3b5daf3fa83d60f314db1baafa6accc2c5217781
Gustavo de Paula <gdepaula@gmail.com>
2021-02-08 11:58:56 -0300

    Variable Mutation and Type Coercion

--- javascript-course/version1/2-JS-basics/script.js
+++ javascript-course/version1/2-JS-basics/script.js
@@ -1,3 +1,8 @@
+/*****************************************
+ * Vabriables and data types
+ */
+
+/*
 var firstName = "Gustavo";
 console.log(firstName);
 
@@ -13,5 +18,49 @@ console.log(job);
 job = "Frontend";
 console.log(job);
 
+// Variable naming rules
 var _3years = 3;
 var jonhMark = "Jonh and Mark";
+
+*/
+
+/*****************************************
+ * Vabriables mutation and type coercion
+ */
+
+var firstName = "Gustavo";
+var age = 37;
+
+console.log(firstName + " " + age);
+
+// Type coercion
+var job, isMarried;
+job = "Frontend";
+isMarried = true;
+
+console.log(
+  firstName +
+    " is a " +
+    age +
+    " years old " +
+    job +
+    ". Is he married? " +
+    isMarried
+);
+
+// Variable mutation
+age = "thirty seven";
+job = "driver";
+
+console.log(
+  firstName +
+    " is a " +
+    age +
+    " years old " +
+    job +
+    ". Is he married? " +
+    isMarried
+);
+
+var lastName = prompt("What is his lastname?");
+console.log(firstName + " " + lastName);

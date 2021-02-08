68d6a0b29afc918f2a60afd757a3b709b5692f37
Gustavo de Paula <gdepaula@gmail.com>
2021-02-08 11:43:53 -0300

    Variables and Data Types

--- javascript-course/version1/2-JS-basics/script.js
+++ javascript-course/version1/2-JS-basics/script.js
@@ -1,1 +1,17 @@
-console.log("Hello world");
+var firstName = "Gustavo";
+console.log(firstName);
+
+var lastName = "de Paula";
+var age = 37;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = "Frontend";
+console.log(job);
+
+var _3years = 3;
+var jonhMark = "Jonh and Mark";

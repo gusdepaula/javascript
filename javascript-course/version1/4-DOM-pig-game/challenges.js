9ffbc13436980621ce394653a38935aa393d3d65
Gustavo de Paula <gdepaula@gmail.com>
2021-02-17 12:42:50 -0300

    Coding Challenge 3: Solution, Part 2

--- javascript-course/version1/4-DOM-pig-game/challenges.js
+++ javascript-course/version1/4-DOM-pig-game/challenges.js
@@ -52,8 +52,19 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
     document.querySelector(`#score-${activePlayer}`).textContent =
       scores[activePlayer];
 
+    let input = document.querySelector(".final-score").value;
+    let winningScore;
+
+    // Undefined, 0, null or "" are coerced to false
+    // Anything else is coerced to true
+    if (input) {
+      winningScore = input;
+    } else {
+      winningScore = 100;
+    }
+
     // Check if player won the game
-    if (scores[activePlayer] > 100) {
+    if (scores[activePlayer] > winningScore) {
       document.querySelector(`#name-${activePlayer}`).textContent = "Winner!";
       document.querySelector(".dice").style.display = "none";
       document

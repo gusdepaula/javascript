e8bc93a1eca573e66b36c8f18002cb47703375f6
Gustavo de Paula <gdepaula@gmail.com>
2021-02-18 15:07:48 -0300

    First Class Functions: Passing Functions as Arguments

--- javascript-course/version1/5-advanced-JS/script.js
+++ javascript-course/version1/5-advanced-JS/script.js
@@ -1,3 +1,4 @@
+/////////////////////////////
 // Function Constructor
 /* 
 var john = {
@@ -30,6 +31,7 @@ console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName); */
 
+/////////////////////////////
 // Object.create
 /* 
 var personProto = {
@@ -49,8 +51,9 @@ var jane = Object.create(personProto, {
   job: { value: "designer" },
 }); */
 
+/////////////////////////////
 // Primitives vs. Objects
-
+/* 
 // Primitives
 var a = 23;
 var b = a;
@@ -83,3 +86,40 @@ function change(a, b) {
 change(age, obj);
 console.log(age);
 console.log(obj.city);
+ */
+
+/////////////////////////////
+// Passing functions as arguments
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
+}
+
+function calculateAge(el) {
+  return new Date().getFullYear() - el;
+}
+
+function isFullAge(el) {
+  return el >= 18;
+}
+
+function maxHeartRate(el) {
+  if (el >= 18 && el <= 81) {
+    return Math.round(206.9 - 0.67 * el);
+  } else {
+    return -1;
+  }
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+console.log(ages);
+console.log(fullAges);
+console.log(rates);

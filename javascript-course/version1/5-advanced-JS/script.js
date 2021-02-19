48bed89bcd200377da582bab5e0f3c4ae4f308d0
Gustavo de Paula <gdepaula@gmail.com>
2021-02-19 11:24:17 -0300

    First Class Functions: Functions Returning Functions

--- javascript-course/version1/5-advanced-JS/script.js
+++ javascript-course/version1/5-advanced-JS/script.js
@@ -91,7 +91,7 @@ console.log(obj.city);
 /////////////////////////////
 // Passing functions as arguments
 
-var years = [1990, 1965, 1937, 2005, 1998];
+/* var years = [1990, 1965, 1937, 2005, 1998];
 
 function arrayCalc(arr, fn) {
   var arrRes = [];
@@ -122,4 +122,34 @@ var fullAges = arrayCalc(ages, isFullAge);
 var rates = arrayCalc(ages, maxHeartRate);
 console.log(ages);
 console.log(fullAges);
-console.log(rates);
+console.log(rates); */
+
+/////////////////////////////
+// Functions Returning Functions
+
+function interviewQuestion(job) {
+  if (job === "designer") {
+    return function (name) {
+      console.log(`${name}, can you please explain what UX design is?`);
+    };
+  } else if (job === "teacher") {
+    return function (name) {
+      console.log(`What subject do you teach, ${name}?`);
+    };
+  } else {
+    return function (name) {
+      console.log(`Hello ${name}, what do you do?`);
+    };
+  }
+}
+
+var teacherQuestion = interviewQuestion("teacher");
+var designerQuestion = interviewQuestion("designer");
+
+teacherQuestion("John");
+designerQuestion("John");
+designerQuestion("Jane");
+designerQuestion("Mark");
+designerQuestion("Mike");
+
+interviewQuestion("teacher")("Mark");

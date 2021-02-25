088e460cb95caa90ed9693265beb71dd79009621
Gustavo de Paula <gdepaula@gmail.com>
2021-02-25 11:27:53 -0300

    Updating the Budget: Controller

--- javascript-course/version1/6-budgety/app.js
+++ javascript-course/version1/6-budgety/app.js
@@ -72,7 +72,7 @@ var UIController = (function () {
       return {
         type: document.querySelector(DOMstrings.inputType).value,
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value,
+        value: Number(document.querySelector(DOMstrings.inputValue).value),
       };
     },
     addListItem: function (obj, type) {
@@ -92,7 +92,7 @@ var UIController = (function () {
       // Replace the placeholder text with some actual data
       newHtml = html.replace("%id%", obj.id);
       newHtml = newHtml.replace("%description%", obj.description);
-      newHtml = newHtml.replace("%value%", (obj.value, type));
+      newHtml = newHtml.replace("%value%", obj.value, type);
 
       // Insert the HTML into the DOM
       document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
@@ -132,23 +132,33 @@ var controller = (function (budgetCtrl, UICtrl) {
     });
   };
 
+  var updateBudget = function () {
+    // 1. Calcute the budget
+    // 2. return the budget
+    // 3. Display the budget on the UI
+  };
+
   var ctrlAddItem = function () {
     var input, newItem;
 
     // 1. Get the filed input data
     input = UICtrl.getInput();
 
-    // 2. Add the item to the budget controller
-    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+    if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+      // 2. Add the item to the budget controller
+      newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-    // 3. Add the item to the UI
-    UICtrl.addListItem(newItem, input.type);
+      // 3. Add the item to the UI
+      UICtrl.addListItem(newItem, input.type);
 
-    // 4. Clear the fields
-    UICtrl.clearFields();
+      // 4. Clear the fields
+      UICtrl.clearFields();
 
-    // 5. Calculate the budget
-    // 6 . Display the budget on the UI
+      // 5. Calculate and Update the budget
+      updateBudget();
+
+      // 6 . Display the budget on the UI
+    }
   };
 
   return {

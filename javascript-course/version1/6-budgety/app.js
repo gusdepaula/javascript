e1efed3470c343bbc07f22040158c37cf7be5676
Gustavo de Paula <gdepaula@gmail.com>
2021-02-26 11:14:48 -0300

    Deleting an Item from Our Budget ControllerCar

--- javascript-course/version1/6-budgety/app.js
+++ javascript-course/version1/6-budgety/app.js
@@ -60,6 +60,17 @@ var budgetController = (function () {
       //Return the new element
       return newItem;
     },
+    deleteItem: function (type, id) {
+      var ids, index;
+      // id = 3
+      ids = data.allItems[type].map(function (current) {
+        return current.id;
+      });
+      index = ids.indexOf(id);
+      if (index !== -1) {
+        data.allItems[type].splice(index, 1);
+      }
+    },
     calculateBudget: function () {
       // Calculate total income and expenses
       calculateTotal("exp");
@@ -229,9 +240,10 @@ var controller = (function (budgetCtrl, UICtrl) {
       // inc-1
       splitID = itemID.split("-");
       type = splitID[0];
-      ID = splitID[1];
+      ID = Number(splitID[1]);
 
       // 1. Delete the item from the data structure
+      budgetCtrl.deleteItem(type, ID);
 
       // 2. Delete the item from the UI
 

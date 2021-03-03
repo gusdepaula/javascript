f191715970682d1e0ab7991c219a42b5617371da
Gustavo de Paula <gdepaula@gmail.com>
2021-03-03 12:01:01 -0300

    Destructuring

--- javascript-course/version1/7-ES6/script.js
+++ javascript-course/version1/7-ES6/script.js
@@ -162,7 +162,7 @@ const box6 = {
 };
 
 box6.clickMe();
- */
+
 
 function Person(name) {
   this.name = name;
@@ -188,3 +188,39 @@ Person.prototype.myFriends6 = function (friends) {
 };
 
 new Person("Mike").myFriends6(friends);
+ */
+
+///////////////////////////////////////////
+// Lecture: Arrow Functions: Destructuring
+
+// ES5
+var jonh = ["John", 26];
+// var name = john[0];
+// var age = jonh[1];
+
+// ES6
+const [name, age] = ["John", 26];
+console.log(name);
+console.log(age);
+
+const obj = {
+  firstName: "Jonh",
+  lastName: "Smith",
+};
+
+const { firstName, lastName } = obj;
+console.log(firstName);
+console.log(lastName);
+
+const { firstName: a, lastName: b } = obj;
+console.log(a);
+console.log(b);
+
+function calcAgeRetirement(year) {
+  const age = new Date().getFullYear() - year;
+  return [age, 65 - age];
+}
+
+const [age, retirement] = calcAgeRetirement(1990);
+console.log(age);
+console.log(retirement);

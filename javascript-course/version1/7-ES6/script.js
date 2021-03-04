6c6d7b3766a83e37822731e41e1c0c5012a8acf7
Gustavo de Paula <gdepaula@gmail.com>
2021-03-04 15:44:24 -0300

    Maps

--- javascript-course/version1/7-ES6/script.js
+++ javascript-course/version1/7-ES6/script.js
@@ -390,7 +390,6 @@ isFullAge6(16, 1990, 1999, 1965);
   this.yearOfBirth = yearOfBirth;
   this.nationality = nationality;
 }
- */
 
 // ES6
 function SmithPerson(
@@ -406,3 +405,43 @@ function SmithPerson(
 }
 var john = new SmithPerson("John", 1990);
 var emily = new SmithPerson("Emily", 1983, "Diaz", "spanish");
+ */
+
+///////////////////////////////////////////
+// Lecture: Maps
+
+const question = new Map();
+question.set(
+  "question",
+  "What is the official name of the latest major Javascript version?"
+);
+question.set(1, "ES5");
+question.set(2, "ES6");
+question.set(3, "ES2015");
+question.set(4, "ES7");
+question.set("correct", 3);
+question.set(true, "Correct answer 😀");
+question.set(false, "Wrong, please try again!");
+
+console.log(question.get("question"));
+// console.log(question.size);
+
+if (question.has(4)) {
+  //   question.delete(4);
+  //   console.log("Answer 4 is here");
+}
+
+// question.clear();
+
+// question.forEach((value, key) =>
+//   console.log(`this is ${key} and it's set to ${value}`)
+// );
+
+for (let [key, value] of question.entries()) {
+  if (typeof key === "number") {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+
+const ans = Number(prompt("Write the correct answer"));
+console.log(question.get(ans === question.get("correct")));

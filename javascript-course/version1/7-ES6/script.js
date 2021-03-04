46ed24a37f6779cc3aaa25b25d657e403a918432
Gustavo de Paula <gdepaula@gmail.com>
2021-03-04 14:31:17 -0300

    The Spread Operator

--- javascript-course/version1/7-ES6/script.js
+++ javascript-course/version1/7-ES6/script.js
@@ -228,7 +228,7 @@ console.log(retirement);
 
 ///////////////////////////////////////////
 // Lecture: Arrays in ES6/ES2015
-
+/* 
 const boxes = document.querySelectorAll(".box");
 
 // ES5
@@ -243,12 +243,12 @@ const boxesArr6 = Array.from(boxes);
 boxesArr6.forEach((cur) => (cur.style.backgroundColor = "dodgerblue"));
 
 // ES5
-/* for (var i = 0; i < boxesArr5.length; i++) {
+for (var i = 0; i < boxesArr5.length; i++) {
   if (boxesArr5[i].className === c) {
     continue;
   }
   boxesArr5[i].textContent = "I changed to blue!";
-} */
+}
 
 // ES6
 for (const cur of boxesArr6) {
@@ -271,3 +271,34 @@ console.log(ages[fullAge.indexOf(true)]);
 // ES6
 console.log(ages.findIndex((cur) => cur >= 18));
 console.log(ages.find((cur) => cur >= 18));
+ */
+
+///////////////////////////////////////////
+// The Spread Operator
+
+function addFourAges(a, b, c, d) {
+  return a + b + c + d;
+}
+
+var sum1 = addFourAges(18, 30, 12, 21);
+console.log(sum1);
+
+// ES5
+var ages = [18, 30, 12, 21];
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2);
+
+// ES6
+const sum3 = addFourAges(...ages);
+console.log(sum3);
+
+const familySmith = ["John", "Jane", "Mark"];
+const familyMiller = ["Mary", "Bob", "Ann"];
+const bigFamily = [...familySmith, "Lily", ...familyMiller];
+console.log(bigFamily);
+
+const h = document.querySelector("h1");
+const boxes = document.querySelectorAll(".box");
+const all = [h, ...boxes];
+
+Array.from(all).forEach((cur) => (cur.style.color = "purple"));

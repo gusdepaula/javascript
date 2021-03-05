4b24ed4f9b686a0d682e703ccf9a06c9b1f6b85f
Gustavo de Paula <gdepaula@gmail.com>
2021-03-05 14:29:05 -0300

    Classes

--- javascript-course/version1/7-ES6/script.js
+++ javascript-course/version1/7-ES6/script.js
@@ -410,7 +410,7 @@ var emily = new SmithPerson("Emily", 1983, "Diaz", "spanish");
 ///////////////////////////////////////////
 // Lecture: Maps
 
-const question = new Map();
+/* const question = new Map();
 question.set(
   "question",
   "What is the official name of the latest major Javascript version?"
@@ -445,3 +445,42 @@ for (let [key, value] of question.entries()) {
 
 const ans = Number(prompt("Write the correct answer"));
 console.log(question.get(ans === question.get("correct")));
+ */
+
+///////////////////////////////////////////
+// Lecture: Classes
+
+// ES5
+var Person5 = function (name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+};
+
+Person5.prototype.calculateAge = function () {
+  var age = new Date().getFullYear() - this.yearOfBirth;
+  console.log(age);
+};
+
+var jonh5 = new Person5("John", 1990, "teacher");
+
+class Person6 {
+  constructor(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+  }
+
+  calculateAge() {
+    var age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+  }
+
+  static greeting() {
+    console.log(`Hey there!`);
+  }
+}
+
+const john6 = new Person6("John", 1990, "teacher");
+
+Person6.greeting();

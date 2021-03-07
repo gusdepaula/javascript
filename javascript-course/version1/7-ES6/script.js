12f7f0e2ebf87df2b553ba250f2ebdc9b82208f2
Gustavo de Paula <gdepaula@gmail.com>
2021-03-07 11:01:58 -0300

    Coding Challenge 5: Solution

--- javascript-course/version1/7-ES6/script.js
+++ javascript-course/version1/7-ES6/script.js
@@ -573,3 +573,98 @@ All the report data should be printed to the console.
 HINT: Use some of the ES6 features: classes, subclasses, template strings, default parameters, maps, arrow functions, destructuring, etc.
 
 */
+
+class Element {
+  constructor(name, buildYear) {
+    this.name = name;
+    this.buildYear = buildYear;
+  }
+}
+
+class Park extends Element {
+  constructor(name, buildYear, area, numTrees) {
+    super(name, buildYear);
+    this.area = area;
+    this.numTrees = numTrees;
+  }
+
+  treeDensity() {
+    const density = this.numTrees / this.area;
+    console.log(
+      `${this.name} has a tree density of ${Math.round(
+        density
+      )} trees per square km.`
+    );
+  }
+}
+
+class Street extends Element {
+  constructor(name, buildYear, length, size = 3) {
+    super(name, buildYear);
+    this.length = length;
+    this.size = size;
+  }
+
+  classifyStreet() {
+    const classification = new Map();
+    // tiny/small/normal/big/huge
+    classification.set(1, "tiny");
+    classification.set(2, "small");
+    classification.set(3, "normal");
+    classification.set(4, "big");
+    classification.set(5, "huge");
+    console.log(
+      `${this.name}, build in ${this.buildYear}, is a ${classification.get(
+        this.size
+      )} street.`
+    );
+  }
+}
+
+const allParks = [
+  new Park("Green Park", 1987, 0.2, 215),
+  new Park("National Park", 1894, 2.9, 3541),
+  new Park("Oak Park", 1953, 0.4, 949),
+];
+
+const allStreets = [
+  new Street("Ocean Avenue", 1999, 1.1, 4),
+  new Street("Evergreen Street", 2008, 2.7, 2),
+  new Street("4th Street", 2015, 0.8),
+  new Street("Sunset Boulevard", 1982, 2.5, 5),
+];
+
+function calc(arr) {
+  const sum = arr.reduce((prev, cur, index) => prev + cur, 0);
+
+  return [sum, sum / arr.length];
+}
+
+function reportParks(p) {
+  console.log("-----PARKS REPORT-----");
+
+  // Density
+  p.map((cur) => cur.treeDensity());
+
+  // Avarage age
+  const ages = p.map((cur) => new Date().getFullYear() - cur.buildYear);
+  const [totalAge, avgAge] = calc(ages);
+  console.log(
+    `Our ${p.length} parks have an average of ${Math.round(avgAge)} years.`
+  );
+
+  // Which park has more than 1000 trees
+  const i = p.map((cur) => cur.numTrees).findIndex((cur) => cur >= 1000);
+  console.log(`${p[i].name} has more than 1000 trees`);
+}
+
+function reportStreets(s) {
+  console.log("-----STREETS REPORT-----");
+
+  // Total and average length of the town's streets
+
+  // CLassify sizes
+}
+
+reportParks(allParks);
+reportStreets(allStreets);

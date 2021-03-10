d00a16e18f8f314506a5ec68e47ef8b0370b583f
Gustavo de Paula <gdepaula@gmail.com>
2021-03-10 15:06:28 -0300

    Building The Search Model

--- javascript-course/version1/9-forkify/src/js/index.js
+++ javascript-course/version1/9-forkify/src/js/index.js
@@ -1,23 +1,5 @@
-import axios from "axios";
+import Search from "./models/Search";
 
-async function getResults(query) {
-  /* try {
-    const key = "57b204b5-b9a2-40db-964c-ff5f543d42dc";
-    const res = await axios(
-      `https://forkify-api.herokuapp.com/api/v2/recipes?search=${query}&key=${key}`
-    );
-    console.log(res);
-    // const recipes = await res.data.data.recipes;
-  } catch (error) {
-    console.log(error);
-  } */
-  try {
-    const res = await axios(`data.json?search=${query}`);
-    console.log(res);
-    const recipes = await res.data.data.recipes;
-    console.log(recipes);
-  } catch (error) {
-    console.log(error);
-  }
-}
-getResults("pizza");
+const search = new Search("pizza");
+console.log(search);
+search.getResults();
--- javascript-course/version1/9-forkify/src/js/models/Search.js
+++ javascript-course/version1/9-forkify/src/js/models/Search.js
@@ -1,1 +1,16 @@
-export default "I am an exported string.";
+import axios from "axios";
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+  async getResults(query) {
+    try {
+      const res = await axios(`data.json?search=${this.query}`);
+      this.result = res.data.data.recipes;
+      // console.log(this.result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}

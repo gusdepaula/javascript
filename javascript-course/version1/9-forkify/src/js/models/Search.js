ea689b6984ff1852e61672f97877c0213f38ef93
Gustavo de Paula <gdepaula@gmail.com>
2021-03-11 13:18:35 -0300

    Implementing Search Results Pagination

--- javascript-course/version1/9-forkify/src/js/models/Search.js
+++ javascript-course/version1/9-forkify/src/js/models/Search.js
@@ -9,21 +9,14 @@ export default class Search {
       const res = await axios(`data.json?search=${this.query}`);
       this.result = res.data.data.recipes;
 
-      this.filteredQuery = this.result.filter((item) =>
-        item.ingredients.find((ingredient) => {
-          const ingDescLowerCase = ingredient.description.toLowerCase();
-          const queryLowerCase = this.query.toLowerCase();
-          if (ingDescLowerCase === queryLowerCase) {
-            return true;
-          } else {
-            return false;
-          }
-        })
-      );
+      this.filteredQuery = this.result.filter((item) => {
+        const arrTitle = [item.title.toLowerCase().split(" ")];
+        const queryLowerCase = this.query.toLowerCase();
 
-      //   console.log(this.result);
-
-      //   console.log(this.filteredQuery);
+        const filter = arrTitle.find((ing) => ing.includes(queryLowerCase));
+        // console.log(filter);
+        return filter;
+      });
     } catch (error) {
       console.log(error);
     }

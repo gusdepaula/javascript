5fdf8271b170e00b91b8cf644b2d383d858ef5bb
Gustavo de Paula <gdepaula@gmail.com>
2021-03-11 10:51:45 -0300

    Adds the query filter

--- javascript-course/version1/9-forkify/src/js/index.js
+++ javascript-course/version1/9-forkify/src/js/index.js
@@ -13,7 +13,7 @@ const state = {};
 const controlSearch = async () => {
   // 1) Get query from view
   const query = searchView.getInput();
-  console.log(query);
+  //   console.log(query);
 
   if (query) {
     // 2) New search object and add to state
@@ -27,7 +27,7 @@ const controlSearch = async () => {
     await state.search.getResults();
 
     // 5) Render results on UI
-    searchView.renderResults(state.search.result);
+    searchView.renderResults(state.search.filteredQuery);
   }
 };
 
--- javascript-course/version1/9-forkify/src/js/models/Search.js
+++ javascript-course/version1/9-forkify/src/js/models/Search.js
@@ -4,11 +4,26 @@ export default class Search {
   constructor(query) {
     this.query = query;
   }
-  async getResults(query) {
+  async getResults() {
     try {
       const res = await axios(`data.json?search=${this.query}`);
       this.result = res.data.data.recipes;
-      // console.log(this.result);
+
+      this.filteredQuery = this.result.filter((item) =>
+        item.ingredients.find((ingredient) => {
+          const ingDescLowerCase = ingredient.description.toLowerCase();
+          const queryLowerCase = this.query.toLowerCase();
+          if (ingDescLowerCase === queryLowerCase) {
+            return true;
+          } else {
+            return false;
+          }
+        })
+      );
+
+      //   console.log(this.result);
+
+      //   console.log(this.filteredQuery);
     } catch (error) {
       console.log(error);
     }

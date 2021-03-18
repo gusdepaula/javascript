7e65e9c83dab1e9d693850b4b66682e4aab41b11
Gustavo de Paula <gdepaula@gmail.com>
2021-03-18 16:51:36 -0300

    Implementing Firebase Database

--- javascript-course/version1/9-forkify/src/js/models/Search.js
+++ javascript-course/version1/9-forkify/src/js/models/Search.js
@@ -8,7 +8,43 @@ export default class Search {
   async getResults() {
     try {
       const res = await axios(`data.json?search=${this.query}`);
+
+      const query2 = this.query;
+      // Initialize Firebase
+      const config = {
+        apiKey: "AIzaSyDmCKgFv0fLtj6-pNlxczZe_wCMYxY1dYQ",
+        authDomain: "doacoes-cdff8.firebaseio.com",
+        databaseURL: "https://doacoes-cdff8.firebaseio.com",
+        projectId: "doacoes-cdff8",
+      };
+      firebase.initializeApp(config);
+
+      let filteredQuery;
+
+      //Create a node at firebase location to add locations as child keys
+      firebase
+        .database()
+        .ref("data")
+        .on("value", (snapshot) => {
+          const data = snapshot.val().recipes;
+          filteredQuery = data.filter((item) => {
+            const arrTitle = [item.title.toLowerCase().split(" ")];
+            const queryLowerCase = query2.toLowerCase();
+
+            const filter = arrTitle.find((ing) => ing.includes(queryLowerCase));
+            return filter;
+          });
+          console.log(filteredQuery);
+          return filteredQuery;
+        });
+
       this.result = res.data.data.recipes;
+
+      this.recipes = filteredQuery;
+
+      console.log(teste);
+
+      // this.filteredQuery = ;
 
       this.filteredQuery = this.result.filter((item) => {
         const arrTitle = [item.title.toLowerCase().split(" ")];

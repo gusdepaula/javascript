9d995db496572a3895dcbcb3fdc5c4f5ea8458b3
Gustavo de Paula <gdepaula@gmail.com>
2021-03-24 12:02:38 -0300

    Change Firebase Account

--- javascript-course/version1/9-forkify/src/js/views/config.js
+++ javascript-course/version1/9-forkify/src/js/views/config.js
@@ -1,8 +1,8 @@
 const config = {
-  apiKey: "AIzaSyDmCKgFv0fLtj6-pNlxczZe_wCMYxY1dYQ",
-  authDomain: "doacoes-cdff8.firebaseio.com",
-  databaseURL: "https://doacoes-cdff8.firebaseio.com",
-  projectId: "doacoes-cdff8",
+  apiKey: "AIzaSyDF3DS6NdYxslX_7RaqLGg025GFCAdWiU8",
+  authDomain: "veganrecipes-7314c-default-rtdb.firebaseio.com/",
+  databaseURL: "https://veganrecipes-7314c-default-rtdb.firebaseio.com/",
+  projectId: "veganrecipes-7314c",
 };
 
 export const fb = firebase.initializeApp(config);

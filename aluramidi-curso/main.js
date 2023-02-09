481a981ebba2306ad07d7b780c6b05864f0d428f
Gustavo de Paula <gdepaula@gmail.com>
2023-02-09 12:41:12 -0300

    Funções anônimas

--- aluramidi-curso/main.js
+++ aluramidi-curso/main.js
@@ -7,6 +7,8 @@ const listaTeclas = document.querySelectorAll(".tecla");
 let contador = 0;
 
 while (contador < listaTeclas.length) {
-  listaTeclas[contador].addEventListener("click", tocaSomPom);
+  listaTeclas[contador].addEventListener("click", function () {
+    tocaSom();
+  });
   contador++;
 }

71ac0eb4a2968ffcb0aaf5f1944908b4b55ea313
Gustavo de Paula <gdepaula@gmail.com>
2023-02-10 11:49:29 -0300

    Adicionando removendo classe

--- aluramidi-curso/main.js
+++ aluramidi-curso/main.js
@@ -13,6 +13,13 @@ for (let index = 0; index < listaTeclas.length; index++) {
 
   tecla.addEventListener("click", function () {
     tocaSom(idAudio);
-    this.classList.toogle("ativa");
   });
+
+  tecla.onkeydown = function () {
+    tecla.classList.add("ativa");
+  };
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
 }
